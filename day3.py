90b519b152ac66fa8f7f66e500f683cb74213a64
Elvira_Kurmanbekovae <elvira.kurmanbekova.ch@gmail.com>
2023-11-11 21:45:59 +0600

    Add new class

---
+++ day3.py
@@ -0,0 +1,11 @@
+class Person:
+    
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+
+
+
+person_1 = Person("Alex", "Baker")
+print(person_1.name)
+print(person_1.surname)
